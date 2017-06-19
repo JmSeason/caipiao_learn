18c9fc75411943677763988705406e03586d4bce
JmSeason <JmYangSeason@hotmail.com>
2017-06-19 18:16:08 +0800

    training change

--- training.py
+++ training.py
@@ -5,11 +5,11 @@ import data_source
 def train_7lc():
     ds = data_source.get_7lc_winning_numbers()
     x_train, y_train = data_transform.increasing_segmentation_transform(ds,
-                                                                        segmentation_span=33,
+                                                                        segmentation_span=20,
                                                                         max_data=30)
-    model = train_model.mlp(x_train, y_train, hidden_layer_nodes=(100, 100),
+    model = train_model.mlp(x_train, y_train, hidden_layer_nodes=(2000, 300),
                             output_layer_activation='sigmoid')
-    model.train()
+    model.train(epochs=10, batch_size=2)
 
 
 
